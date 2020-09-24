3a56426e4e99630f97e4faaf0f1fb082575f6be2
王文超 <wangwenchao@pcitech.com>
2020-09-24 16:14:38 +0800

    删除部分无用代码

--- src/Hjmos.CommonControls/Models/VideoMonitor.cs
+++ src/Hjmos.CommonControls/Models/VideoMonitor.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Prism.Mvvm;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hjmos.CommonControls
 {
