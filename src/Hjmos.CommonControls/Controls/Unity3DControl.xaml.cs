b105a9d593cf83a52ec7a7ec7e81502a5feddd1a
王文超 <wangwenchao@pcitech.com>
2020-09-25 10:08:56 +0800

    增加Unity3D控件

---
+++ src/Hjmos.CommonControls/Controls/Unity3DControl.xaml.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Hjmos.CommonControls.Controls
+{
+    /// <summary>
+    /// Unity3DControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class Unity3DControl : UserControl
+    {
+        public Unity3DControl()
+        {
+            InitializeComponent();
+            this.Loaded += Unity3DControlExtent_Loaded;
+            this.IsVisibleChanged += Unity3DControl_IsVisibleChanged;
+        }
+        private void Panel_SizeChanged(object sender, EventArgs e)
+        {
+            if (unityHWND != IntPtr.Zero)
+            {
+                WinApiHelper.MoveWindow(unityHWND, 0, 0, Panel.Width, Panel.Height, true);
+            }
+        }
+
+
+        /// <summary>
+        /// 承载的窗体关闭时，关闭当前3D的进程
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Unity3DControl_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Close3DProcess();
+        }
+
+        private void Unity3DControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Start3DProcess();
+        }
+
+        private bool IsFirstLoad = false;
+
+        private void Unity3DControlExtent_Loaded(object sender, RoutedEventArgs e)
+        {
+            //注册监听窗体关闭的事件
+            if (IsVisible)
+            {
+                if (!IsFirstLoad)
+                {
+                    Window.GetWindow(this).Closing -= Unity3DControl_Closing;
+                    Window.GetWindow(this).Closing += Unity3DControl_Closing;
+                    IsFirstLoad = true;
+                }
+
+                if (unityHWND != IntPtr.Zero)
+                {
+                    WinApiHelper.MoveWindow(unityHWND, 0, 0, Panel.Width, Panel.Height, true);
+                }
+            }
+        }
+
+        private Process process;
+        private IntPtr unityHWND = IntPtr.Zero;
+        private const int WM_ACTIVATE = 0x0006;
+        private readonly IntPtr WA_ACTIVE = new IntPtr(1);
+        private readonly IntPtr WA_INACTIVE = new IntPtr(0);
+
+        /// <summary>
+        /// 当前3D的句柄
+        /// </summary>
+        public IntPtr CurrentUnity3DIntPtr
+        {
+            get { return unityHWND; }
+        }
+
+        /// <summary>
+        /// 寄宿3D的主容器句柄
+        /// </summary>
+        public IntPtr BaseUnity3dParentHost
+        {
+            get { return Panel.Handle; }
+        }
+
+        /// <summary>
+        /// 是否终止界面焦点检测轮训
+        /// </summary>
+        internal bool isStopCheckWindowActive
+        {
+            set
+            {
+                if (value)
+                {
+                    actionTime?.Stop();
+                }
+                else
+                {
+                    if (actionTime == null)
+                    {
+                        CheckWindowsActive();
+                    }
+                    actionTime?.Stop();
+                    actionTime?.Start();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Unity3D的进程参数，用于给3D程序传递参数
+        /// </summary>
+        public string Unity3DProcessArges
+        {
+            get { return (string)GetValue(Unity3DProcessArgesProperty); }
+            set { SetValue(Unity3DProcessArgesProperty, value); }
+        }
+
+        /// <summary>
+        /// 3D进程参数的依赖属性,默认值为空字符串
+        /// </summary>
+        public static readonly DependencyProperty Unity3DProcessArgesProperty = DependencyProperty.Register("Unity3DProcessArges", typeof(string), typeof(Unity3DControl), new PropertyMetadata(string.Empty));
+
+
+        /// <summary>
+        /// 3D进程地址依赖属性
+        /// </summary>
+        public static readonly DependencyProperty Unity3DProcessUriProperty = DependencyProperty.Register("Unity3DProcessUri", typeof(string), typeof(Unity3DControl), new PropertyMetadata(null, new PropertyChangedCallback(Unity3DProcessUriPropertyChange)));
+
+
+        /// <summary>
+        /// 3D的进程路径改变触发回调
+        /// </summary>
+        /// <param name="dependencyObject"></param>
+        /// <param name="e"></param>
+        private static void Unity3DProcessUriPropertyChange(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            if (dependencyObject is Unity3DControl control)
+            {
+                string OldProcessUri = (string)e.OldValue;
+                string NewProcessUri = (string)e.NewValue;
+
+                control.Old3DProcessName = OldProcessUri;
+                if (OldProcessUri != NewProcessUri)
+                {
+                    control.unityHWND = IntPtr.Zero;
+                    control.Current3DProcessName = NewProcessUri;
+                    control.CheckProcessByName(OldProcessUri);  //先把就的3D进程关闭
+                    control.Start3DProcess();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 当前创建3D的进程名称
+        /// </summary>
+        public string Unity3DProcessUri
+        {
+            get { return (string)GetValue(Unity3DProcessUriProperty); }
+            set { SetValue(Unity3DProcessUriProperty, value); }
+        }
+
+        /// <summary>
+        /// 设置当前播放的3D的进程URL
+        /// </summary>
+        public string Current3DProcessName { get; set; }
+
+        /// <summary>
+        /// 记录上一次播放的3d的进程URL
+        /// </summary>
+        public string Old3DProcessName { get; set; }
+
+
+        /// <summary>
+        /// 开启3D进程
+        /// </summary>
+        private void Start3DProcess()
+        {
+            if (IsVisible)
+            {
+                isStopCheckWindowActive = false;
+                if ((!string.IsNullOrWhiteSpace(Current3DProcessName) && Old3DProcessName != Current3DProcessName) || (process != null && process.HasExited))
+                {
+                    Old3DProcessName = Current3DProcessName;
+                    Create3DProcess(Current3DProcessName);
+                }
+            }
+            else
+            {
+                isStopCheckWindowActive = true;
+            }
+        }
+
+        /// <summary>
+        /// 关闭当前3D进程
+        /// </summary>
+        internal void Close3DProcess()
+        {
+            try
+            {
+                if (actionTime != null)
+                {
+                    actionTime.Stop();
+                    actionTime.Tick -= ActionTime_Tick;
+                }
+
+                if (process == null)
+                {
+                    return;
+                }
+                Task.Run(() =>
+                {
+                    // WinApiHelper.PostMessage(unityHWND, 0x0010, IntPtr.Zero, IntPtr.Zero);
+                    if (!process.HasExited)
+                    {
+                        process?.CloseMainWindow();
+                        Thread.Sleep(1000);
+                        while (!process.HasExited)
+                        {
+                            process.Kill();
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+               // LogHelper.Error(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 开启检测鼠标在窗体上的运动
+        /// </summary>
+        DispatcherTimer actionTime;
+
+        /// <summary>
+        ///检测窗体焦点
+        /// </summary>
+        private void CheckWindowsActive()
+        {
+            actionTime = new DispatcherTimer();
+            actionTime.Interval = new TimeSpan(TimeSpan.TicksPerMillisecond * 300);
+            actionTime.Tick += ActionTime_Tick;
+            actionTime.Start();
+        }
+
+        IntPtr lastIntprt = IntPtr.Zero; //获取上一次的鼠标指向的句柄,为了指向相同的位置，重复给窗体发送消息
+        private void ActionTime_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                WinApiHelper.POINT pOINT;
+                bool isSuccess = WinApiHelper.GetCursorPos(out pOINT);
+                var intptr = WinApiHelper.WindowFromPoint(pOINT);
+                if (isSuccess && unityHWND != IntPtr.Zero && lastIntprt != intptr)
+                {
+                    if (intptr == unityHWND)
+                    {
+                        ActiveWindows();
+                    }
+                    else
+                    {
+                        UnActiveWindows();
+                    }
+
+                }
+                lastIntprt = intptr;
+
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Error(ex.ToString());
+            }
+        }
+
+
+        /// <summary>
+        /// 检测现在需要开启的3D进程是否还在运行，如果是，先关闭当前所运行的3D进程
+        /// </summary>
+        /// <param name="path">需要开启的3D的路径</param>
+        internal void CheckProcessByName(string path)
+        {
+            var ProcessName = System.IO.Path.GetFileNameWithoutExtension(path);
+            try
+            {
+                //精确进程名  用GetProcessesByName
+                foreach (Process p in Process.GetProcessesByName(ProcessName))
+                {
+                    if (!p.CloseMainWindow())
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Error(ex.ToString());
+            }
+        }
+
+        //开启3D进程
+        internal void Create3DProcess(string processUri)
+        {
+            if (string.IsNullOrWhiteSpace(processUri) || !File.Exists(processUri))
+            {
+                //LogHelper.Error("Unable to find Unity window,File was not exit");
+                return;
+                //throw new Exception("Unable to find Unity window,File was not exit");
+            }
+            var handle = Panel.Handle;
+
+            // CheckWindowsActive();
+
+            this.Dispatcher.InvokeAsync(() =>
+            {
+                try
+                {
+                    //WaitDialogWindow = new WaitDialogWindow("加载3D模型数据，请稍后！");
+                    // WaitDialogWindow.Show();
+                    //判断当前要启动的进程是否还在启动，如果还在启动，先关闭进程再创建进程
+                    CheckProcessByName(processUri);
+
+                    if (process != null)
+                    {
+                        process.Close();
+                    }
+
+                    //var arg = Unity3DProcessArges;
+                    //var guid = Guid.NewGuid().ToString();
+                    process = new Process();
+                    //process.StartInfo.Arguments = "-parentHWND " + handle.ToInt32() + " " + Environment.CommandLine +" " + $"{guid}" + "," + "1920" + "," + "1080" +","+ "ws://172.23.127.60:8089/webim-server-zyf/websocket/"+ guid;
+                    process.StartInfo.Arguments = "-parentHWND " + handle.ToInt32() + " " + Environment.CommandLine + " " + Unity3DProcessArges.Replace("AutoWidth", Panel.Width.ToString()).Replace("AutoHeight", Panel.Height.ToString());
+
+                    process.StartInfo.FileName = processUri;
+                    process.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(processUri);
+                    process.StartInfo.UseShellExecute = true;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+                    process.WaitForInputIdle();
+                    WinApiHelper.EnumChildWindows(handle, WindowEnum, IntPtr.Zero);
+
+                }
+                catch (Exception ex)
+                {
+                   // LogHelper.Error(ex.ToString());
+                }
+            });
+
+        }
+
+        private int WindowEnum(IntPtr hwnd, IntPtr lparam)
+        {
+            unityHWND = hwnd;
+            //ActiveWindows();
+            StartActiveUnitt3D();
+            return 0;
+        }
+
+        /// <summary>
+        /// 启动的瞬间开始激活3D的状态
+        /// </summary>
+        private void StartActiveUnitt3D()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(1500);
+                ActiveWindows();
+
+                //Dispatcher.Invoke(() =>
+                //{
+                //    WaitDialogWindow?.CloseWaitDialog(1500);
+                //});
+            });
+        }
+
+        /// <summary>
+        /// 发送消息，触发激活当前窗体
+        /// </summary>
+        internal void ActiveWindows()
+        {
+            // WinApiHelper.SendMessage(unityHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+            if (unityHWND != IntPtr.Zero)
+            {
+                WinApiHelper.SendMessage(unityHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+            }
+        }
+
+        /// <summary>
+        /// 解除激活窗体
+        /// </summary>
+        internal void UnActiveWindows()
+        {
+            if (unityHWND != IntPtr.Zero)
+            {
+                WinApiHelper.SendMessage(unityHWND, WM_ACTIVATE, WA_INACTIVE, IntPtr.Zero);
+            }
+        }
+    }
+}
---
+++ src/Hjmos.CommonControls/Tools/WinApiHelper.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace Hjmos.CommonControls
+{
+   public class WinApiHelper
+    {
+        public delegate int WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+       
+        [DllImport("user32.dll")]
+        public static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc func, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint processId);
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")] // TODO: 32/64?
+        public static extern IntPtr GetWindowLongPtr(IntPtr hWnd, Int32 nIndex);
+        public const Int32 GWLP_USERDATA = -21;
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr PostMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, GetWindowCmd uCmd);
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        public static extern IntPtr CreateWindowEx(int dwExStyle,
+            string lpszClassName,
+            string lpszWindowName,
+            int style,
+            int x, int y,
+            int width, int height,
+            IntPtr hwndParent,
+            IntPtr hMenu,
+            IntPtr hInst,
+            [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        [return:MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowWindow([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent); //该api用于嵌入到窗口中运行
+
+        [DllImport("User32.dll")]
+       public static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SetCursorPos(int x, int y);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetCursorPos(out POINT pt);
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        public static extern bool DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+       public static extern IntPtr WindowFromPoint(POINT Point);
+
+        [DllImport("user32.dll")]
+       public static extern IntPtr WindowFromPoint(int xPoint, int yPoint);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccess desiredAccess,bool inheritHandle,int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle([In] IntPtr handle);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtResumeProcess([In] IntPtr processHandle);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtSuspendProcess([In] IntPtr processHandle);
+
+        /// <summary>
+        /// 暂停进程
+        /// </summary>
+        /// <param name="processId"></param>
+        public static void SuspendProcess(int processId)
+        {
+            IntPtr hProc = IntPtr.Zero;
+            try
+            {
+                // Gets the handle to the Process
+                hProc = OpenProcess(ProcessAccess.SuspendResume, false, processId);
+                if (hProc != IntPtr.Zero)
+                    NtSuspendProcess(hProc);
+            }
+            finally
+            {
+                // Don't forget to close handle you created.
+                if (hProc != IntPtr.Zero)
+                    CloseHandle(hProc);
+            }
+        }
+
+        /// <summary>
+        /// 恢复进程
+        /// </summary>
+        /// <param name="processId"></param>
+        public static void ResumeProcess(int processId)
+        {
+            IntPtr hProc = IntPtr.Zero;
+            try
+            {
+                // Gets the handle to the Process
+                hProc = OpenProcess(ProcessAccess.SuspendResume, false, processId);
+                if (hProc != IntPtr.Zero)
+                    NtResumeProcess(hProc);
+            }
+            finally
+            {
+                // Don't forget to close handle you created.
+                if (hProc != IntPtr.Zero)
+                    CloseHandle(hProc);
+            }
+        }
+
+
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+
+        /// <summary>
+        /// The process-specific access rights.
+        /// </summary>
+        [Flags]
+        public enum ProcessAccess : uint
+        {
+            /// <summary>
+            /// Required to terminate a process using TerminateProcess.
+            /// </summary>
+            Terminate = 0x1,
+
+            /// <summary>
+            /// Required to create a thread.
+            /// </summary>
+            CreateThread = 0x2,
+
+            /// <summary>
+            /// Undocumented.
+            /// </summary>
+            SetSessionId = 0x4,
+
+            /// <summary>
+            /// Required to perform an operation on the address space of a process (see VirtualProtectEx and WriteProcessMemory).
+            /// </summary>
+            VmOperation = 0x8,
+
+            /// <summary>
+            /// Required to read memory in a process using ReadProcessMemory.
+            /// </summary>
+            VmRead = 0x10,
+
+            /// <summary>
+            /// Required to write to memory in a process using WriteProcessMemory.
+            /// </summary>
+            VmWrite = 0x20,
+
+            /// <summary>
+            /// Required to duplicate a handle using DuplicateHandle.
+            /// </summary>
+            DupHandle = 0x40,
+
+            /// <summary>
+            /// Required to create a process.
+            /// </summary>
+            CreateProcess = 0x80,
+
+            /// <summary>
+            /// Required to set memory limits using SetProcessWorkingSetSize.
+            /// </summary>
+            SetQuota = 0x100,
+
+            /// <summary>
+            /// Required to set certain information about a process, such as its priority class (see SetPriorityClass).
+            /// </summary>
+            SetInformation = 0x200,
+
+            /// <summary>
+            /// Required to retrieve certain information about a process, such as its token, exit code, and priority class (see OpenProcessToken, GetExitCodeProcess, GetPriorityClass, and IsProcessInJob).
+            /// </summary>
+            QueryInformation = 0x400,
+
+            /// <summary>
+            /// Undocumented.
+            /// </summary>
+            SetPort = 0x800,
+
+            /// <summary>
+            /// Required to suspend or resume a process.
+            /// </summary>
+            SuspendResume = 0x800,
+
+            /// <summary>
+            /// Required to retrieve certain information about a process (see QueryFullProcessImageName). A handle that has the PROCESS_QUERY_INFORMATION access right is automatically granted PROCESS_QUERY_LIMITED_INFORMATION.
+            /// </summary>
+            QueryLimitedInformation = 0x1000,
+
+            /// <summary>
+            /// Required to wait for the process to terminate using the wait functions.
+            /// </summary>
+            Synchronize = 0x100000
+        }
+
+        public enum GetWindowCmd : uint
+        {
+            /// <summary>
+            /// 返回的句柄标识了在Z序最高端的相同类型的窗口。
+            /// 如果指定窗口是最高端窗口，则该句柄标识了在Z序最高端的最高端窗口；
+            /// 如果指定窗口是顶层窗口，则该句柄标识了在z序最高端的顶层窗口：
+            /// 如果指定窗口是子窗口，则句柄标识了在Z序最高端的同属窗口。
+            /// </summary>
+            GW_HWNDFIRST = 0,
+            /// <summary>
+            /// 返回的句柄标识了在z序最低端的相同类型的窗口。
+            /// 如果指定窗口是最高端窗口，则该柄标识了在z序最低端的最高端窗口：
+            /// 如果指定窗口是顶层窗口，则该句柄标识了在z序最低端的顶层窗口；
+            /// 如果指定窗口是子窗口，则句柄标识了在Z序最低端的同属窗口。
+            /// </summary>
+            GW_HWNDLAST = 1,
+            /// <summary>
+            /// 返回的句柄标识了在Z序中指定窗口下的相同类型的窗口。
+            /// 如果指定窗口是最高端窗口，则该句柄标识了在指定窗口下的最高端窗口：
+            /// 如果指定窗口是顶层窗口，则该句柄标识了在指定窗口下的顶层窗口；
+            /// 如果指定窗口是子窗口，则句柄标识了在指定窗口下的同属窗口。
+            /// </summary>
+            GW_HWNDNEXT = 2,
+            /// <summary>
+            /// 返回的句柄标识了在Z序中指定窗口上的相同类型的窗口。
+            /// 如果指定窗口是最高端窗口，则该句柄标识了在指定窗口上的最高端窗口；
+            /// 如果指定窗口是顶层窗口，则该句柄标识了在指定窗口上的顶层窗口；
+            /// 如果指定窗口是子窗口，则句柄标识了在指定窗口上的同属窗口。
+            /// </summary>
+            GW_HWNDPREV = 3,
+            /// <summary>
+            /// 返回的句柄标识了指定窗口的所有者窗口（如果存在）。
+            /// GW_OWNER与GW_CHILD不是相对的参数，没有父窗口的含义，如果想得到父窗口请使用GetParent()。
+            /// 例如：例如有时对话框的控件的GW_OWNER，是不存在的。
+            /// </summary>
+            GW_OWNER = 4,
+            /// <summary>
+            /// 如果指定窗口是父窗口，则获得的是在Tab序顶端的子窗口的句柄，否则为NULL。
+            /// 函数仅检查指定父窗口的子窗口，不检查继承窗口。
+            /// </summary>
+            GW_CHILD = 5,
+            /// <summary>
+            /// （WindowsNT 5.0）返回的句柄标识了属于指定窗口的处于使能状态弹出式窗口（检索使用第一个由GW_HWNDNEXT 查找到的满足前述条件的窗口）；
+            /// 如果无使能窗口，则获得的句柄与指定窗口相同。
+            /// </summary>
+            GW_ENABLEDPOPUP = 6
+        }
+    }
+}
