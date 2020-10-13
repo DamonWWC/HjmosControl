77ecd9095b3283995e5a35f95b3616c17621178d
王文超 <wangwenchao@pcitech.com>
2020-10-13 09:19:36 +0800

    RadioButon控件和ButtonGroup控件制作

---
+++ src/Hjmos.BaseControls/Controls/Button/ButtonGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Hjmos.BaseControls.Controls
+{
+    public class ButtonGroup : ItemsControl
+    {
+        protected override bool IsItemItsOwnContainerOverride(object item) => item is Button || item is RadioButton || item is ToggleButton;
+
+
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Orientation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(ButtonGroup), new PropertyMetadata(default(Orientation)));
+
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            var count = Items.Count;
+            for(var i=0;i<count;i++)
+            {
+                var item = (ButtonBase)Items[i];
+                item.Style = ItemContainerStyleSelector?.SelectStyle(item, this);
+            }
+        }
+    }
+}
---
+++ src/Hjmos.CustomCharts/Controls/OneDayPassengerFlowTrend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hjmos.CustomCharts.Controls
+{
+    
+    public class OneDayPassengerFlowTrend : Control
+    {
+        static OneDayPassengerFlowTrend()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(OneDayPassengerFlowTrend), new FrameworkPropertyMetadata(typeof(OneDayPassengerFlowTrend)));
+        }
+        public OneDayPassengerFlowTrend()
+        {
+
+        }
+    }
+}
