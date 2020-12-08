c6b425da4c5011b697f018a58b5d6de956489144
王文超 <wangwenchao@pcitech.com>
2020-12-08 19:44:55 +0800

    增加了日期时间，下拉日期与时间控件

---
+++ src/Hjmos.BaseControls/Controls/Attach/MemoElement.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Hjmos.BaseControls.Controls
+{
+    public class MemoElement
+    {
+          /// <summary>
+        ///     占位符
+        /// </summary>
+        public static readonly DependencyProperty MemoTextProperty = DependencyProperty.RegisterAttached(
+            "MemoText", typeof(string), typeof(MemoElement), new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetMemoText(DependencyObject element, string value) => element.SetValue(MemoTextProperty, value);
+
+        public static string GetMemoText(DependencyObject element) => (string)element.GetValue(MemoTextProperty);
+    }
+}
