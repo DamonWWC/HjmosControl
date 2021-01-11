d989ae0ef1f13b31c4c46d4ce96648d6e70bb8ba
王文超 <wangwenchao@pcitech.com>
2021-01-11 10:56:02 +0800

    treeview控件设计

---
+++ src/Hjmos.BaseControls/Tools/Converter/TreeViewItemMarginConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Hjmos.BaseControls.Tools.Converter
+{
+    public class TreeViewItemMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var left = 0.0;
+            UIElement element = value as TreeViewItem;
+            while (element != null && element.GetType() != typeof(TreeView))
+            {
+                element = (UIElement)VisualTreeHelper.GetParent(element);
+                if (element is TreeViewItem)
+                    left += 19.0;
+            }
+            return new Thickness(left, 0, 0, 0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
